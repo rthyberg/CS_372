2172ad5c2dae55e27c51fef033fe5194d82ee458
rthyberg <robertthyberg@gmail.com>
2017-02-09 01:41:22 -0800

    finished.. review for errors

--- asg_1/chatclient.c
+++ asg_1/chatclient.c
@@ -1,53 +1,114 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<string.h>
-#include<sys/socket.h>
-#include<sys/types.h>
-#include<netinet/in.h>
-#include<netdb.h>
+/*
+** showip.c -- show IP addresses for a host given on the command line
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdint.h>
 void error(const char *msg) { perror(msg); exit(0); } // Error function used for reporting issues
-
-int main(int argc , char *argv[])
+int sendMsg(int, void*);
+int recvMsg(int, void*, int);
+int main(int argc, char *argv[])
 {
-    int socketFD, portNumber, charsWritten;
-    char* ip;
-    char check[255];
-    struct sockaddr_in serverAddress;
-    struct hostent* serverHostInfo;
-    if(argc == 3) {
-        ip = argv[1];
-        portNumber = atoi(argv[2]);
-    } else{
-       printf("USAGE: %s HOSTNAME PORT_NUM\n", argv[0]);
-       exit(1);
-    }
-    // Set up the address struct for this process
-    memset((char *)&serverAddress, '\0', sizeof(serverAddress)); // Clear out the address struct
-    serverAddress.sin_family = AF_INET; // Create a network-capable socket
-    serverAddress.sin_port = htons(portNumber); // Store the port number
-    serverHostInfo = gethostbyname("localhost"); // Convert the machine name into special form of address
-    if(serverHostInfo == NULL) {
-        fprintf(stderr, "CLIENT: ERROR, no such host\n");
-        exit(2);
-    }
-     memcpy((char*)&serverAddress.sin_addr.s_addr, (char*)serverHostInfo->h_addr, serverHostInfo->h_length); // Co    py in the address
+    struct addrinfo hints, *res;
+    int status, sockfd;
 
-    // Set up the socket
-    socketFD = socket(AF_INET, SOCK_STREAM, 0); // Create the socket
-    if (socketFD < 0) {
-        error("CLIENT: ERROR opening socket");
-        exit(2);
+    if (argc != 3) {
+        fprintf(stderr,"usage: showip hostname port\n");
+        return 1;
     }
- // Connect to server
-    if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) {// Connect socket to address
-        error("CLIENT: ERROR connecting");
-        exit(2);
-    }
-     // handshake
-    charsWritten = send(socketFD, "otp_enc", 8, 0); // Write to the server
-    if (charsWritten < 0) error("CLIENT: ERROR writing to socket");
-    if (charsWritten < strlen(check)) fprintf(stderr, "CLIENT: WARNING: Not all data written to socket!\n");
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC; // AF_INET or AF_INET6 to force version
+    hints.ai_socktype = SOCK_STREAM;
 
+    if ((status = getaddrinfo(argv[1], argv[2], &hints, &res)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+        return 2;
+    }
+    sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    if(connect(sockfd, res->ai_addr, res->ai_addrlen) == -1) {
+       error("FAILED TO CONNECT TO CLIENT: ");
+       }
+    printf("CONNECTED TO HOST: %s on PORT: %s\n", argv[1], argv[2]);
+    char clientMsg[501];
+    char hostMsg[501];
+    char clientName[11]; // 1 extra than max then trim off \n
+    char serverName[11];
+    memset((char*) &clientName, '\0', sizeof clientName);
+    printf("Please enter your name: ");
+    fgets(clientName, 11, stdin);
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF); // portable purge of stdin http://stackoverflow.com/questions/2187474/i-am-not-able-to-flush-stdin
+    sendMsg(sockfd, clientName);
+    memset((char*) &serverName, '\0', sizeof serverName);
+    recvMsg(sockfd, serverName, 11);
+    printf("Connected with %s:\n", serverName);
+    while(1) {
+        memset((char*) &clientMsg, '\0', sizeof clientMsg);
+        memset((char*) &hostMsg, '\0', sizeof hostMsg);
+        printf("%s > ", clientName);
+        fgets(clientMsg, 501, stdin);
+        sendMsg(sockfd, clientMsg);
+        if(strcmp(clientMsg, "\\quit\n") == 0) { // if we fgets a quit then we will return a 0 and break loop
+            printf("Disconnecting from %s.\n", "Host");
+            break;
+        }
+        recvMsg(sockfd, hostMsg, 501);
+        if(strcmp(hostMsg, "\\quit") == 0) {
+            printf("%s has disconnected.\n", serverName);
+            break;
+        }
+        printf("%s > %s\n", serverName, hostMsg);
+    }
+    shutdown(sockfd,2);
+    freeaddrinfo(res); // free the linked list
+    return 0;
+}
+/* Loops recv so al data gets sent*/
+int sendMsg(int sockfd, void* buffer) {
+    int sent = 0; // holds the number of bytes sent
+    int totalSent = 0; // hold the total of bytes sent
+    unsigned short header = strlen(buffer);
+    unsigned short nheader = htons(header); // we are gonna send a 16bit header representing
+    do {                                               // the size of our msg
+        sent = send(sockfd, &nheader, (sizeof(int16_t) - totalSent), 0); // sends the necessary amount of bytes
+        totalSent = totalSent + sent;  // adds the number to total sent
+    } while(totalSent < 2); // if we didnt send all the bytes then loop till we do
+    sent = 0; // reset values for the message send
+    totalSent = 0;
+    do {
+       sent = send(sockfd, buffer, (header-totalSent), 0); //same process but with msg
+       totalSent = totalSent + sent;
+    } while(totalSent < header);
+    return 0;
+}
+int recvMsg(int sockfd, void* buffer, int sizeofBuffer) {
+    int recd = 0;
+    int totalRecd = 0;
+    unsigned short nheader, header;
+    do {
+        recd = recv(sockfd, &nheader, 2-totalRecd, 0);
+        totalRecd = totalRecd + recd;
+    } while(totalRecd < 2);
+    recd = 0;
+    totalRecd = 0;
+    header = ntohs(nheader);
+    //printf("%u: %u\n", header, sizeofBuffer);
+    if(header > sizeofBuffer) {
+        header = sizeofBuffer;
+    }
+    do {
+        recd = recv(sockfd, buffer, (header-totalRecd), 0);
+        totalRecd = totalRecd + recd;
+        //printf("%s :\n", buffer);
+    } while(totalRecd < header);
+    //printf("Header for:%d... %s\n", header, buffer);
     return 0;
 }
--- asg_1/chatserve.py
+++ asg_1/chatserve.py
@@ -1,30 +1,81 @@
 import socketserver
 import signal
 import sys
-
+import string
+import struct
+import socket as so
 def signal_handler(signal, frame):
     print('Press Ctrl+C')
     sys.exit(0)
 
-class MyTCPHandler(socketserver.BaseRequestHandler):
-    """
-    The RequestHandler class for our server.
 
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
-    """
+class MyTCPHandler(socketserver.StreamRequestHandler):
 
     def handle(self):
-        # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(1024).strip()
-        print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
-        # just send back the same data, but upper-cased
-        self.request.sendall(self.data.upper())
+        # self.rfile is a file-like object created by the handler;
+        # we can now use e.g. readline() instead of raw recv() calls
+        headerSize = self.myheader()
+        self.data = ''
+        clientName = self.request.recv(headerSize).decode('utf-8').strip('\n')
+        clientName = clientName[0:10]
+        print("{} has connected!".format(clientName))
+        serv = input("Please enter your name: ")
+        serverName = "".join(serv[0:10])
+        self.mySend(serverName)
+        while True:
+            headerSize = self.myheader()
+            self.data = self.myreceive(headerSize).decode('utf-8').strip('\n')
+            if self.data == '\quit':
+                print("{} has disconnected.\n".format(clientName))
+                self.request.close()
+                break
+            print("{}> {}".format(clientName, self.data))
+            msg = input("{} > ".format(serverName))
+            self.mySend(msg)
+            print(msg)
+            if msg == "\quit":
+                print("Disconnecting from {}".format(clientName))
+                break
+            # Likewise, self.wfile is a file-like object used to write back
+            # to the client
+            #self.wfile.write(self.data.upper())
+        self.request.close()
+
+    def myreceive(self, MSGLEN):
+        chunks = []
+        bytes_recd = 0
+        while bytes_recd < MSGLEN:
+            chunk = self.request.recv((MSGLEN - bytes_recd))
+            if chunk == b'':
+                raise RuntimeError("socket connection broken")
+            chunks.append(chunk)
+            bytes_recd = bytes_recd + len(chunk)
+        return b''.join(chunks)
+
+    def myheader(self):
+        chunks = []
+        bytes_recd = 0
+        while bytes_recd < 2:
+            chunk = self.request.recv((2))
+            if chunk == b'':
+                raise RuntimeError("socket connection broken")
+            chunks.append(chunk)
+            bytes_recd = bytes_recd + len(chunk)
+            bytes_recd += 1
+        mynum = int.from_bytes(b"".join(chunks),'big')
+        return mynum
+
+    def mySend(self, msg):
+        header = (len(msg)).to_bytes(2,'big');
+        bmsg = msg.encode('utf-8')
+        self.request.sendall(header);
+        self.request.sendall(bmsg);
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 50000
+    if len(sys.argv) != 2:
+        print("USAGE: chatserve.py PORT")
+        sys.exit(1)
+    HOST, PORT = socketserver.socket.gethostname(), int(sys.argv[1])
 
     # Create the server, binding to localhost on port 9999
     server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
@@ -33,3 +84,6 @@ if __name__ == "__main__":
     # interrupt the program with Ctrl-C
     print("Connection open on port " + HOST, PORT);
     server.serve_forever()
+
+
+
