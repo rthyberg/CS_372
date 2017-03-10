423d4f8fb57573ac7af701ae8815fa8866fc3fe6
rthyberg <robertthyberg@gmail.com>
2017-03-09 22:26:00 -0800

    idk

--- asg_2/ftclient.py
+++ asg_2/ftclient.py
@@ -1,82 +1,18 @@
-import socketserver
-import signal
+import socket
 import sys
-import string
-import struct
-import socket as so
-def signal_handler(signal, frame):
-    print('Press Ctrl+C')
-    sys.exit(0)
 
+HOST, PORT = sys.argv[1], int(sys.argv[2])
+print(sys.argv[1])
+data = " ".join(sys.argv[1:])
 
-class MyTCPHandler(socketserver.StreamRequestHandler):
+# Create a socket (SOCK_STREAM means a TCP socket)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    # Connect to server and send data
+    sock.connect((HOST, PORT))
+    sock.sendall(bytes(data + "\n", "utf-8"))
 
-    def handle(self):
-        # self.rfile is a file-like object created by the handler;
-        # we can now use e.g. readline() instead of raw recv() calls
-        headerSize = self.myheader()
-        self.data = ''
-        clientName = self.request.recv(headerSize).decode('utf-8').strip('\n') # strip and convert into string
-        clientName = clientName[0:10]
-        print("{} has connected!".format(clientName))
-        serv = input("Please enter your name: ") # get server handle
-        serverName = "".join(serv[0:10])
-        self.mySend(serverName)
-        while True: # get messages
-            headerSize = self.myheader()
-            self.data = self.myreceive(headerSize).decode('utf-8').strip('\n')
-            if self.data == '\quit':
-                print("{} has disconnected.\n".format(clientName))
-                self.request.close()
-                break
-            print("{}> {}".format(clientName, self.data))
-            msg = input("{} > ".format(serverName))
-            self.mySend(msg)
-            if msg == "\quit":
-                print("Disconnecting from {}".format(clientName))
-                break
-        self.request.close()
+    # Receive data from the server and shut down
+    received = str(sock.recv(1024), "utf-8")
 
-    def myreceive(self, MSGLEN): # my receive all
-        chunks = []
-        bytes_recd = 0
-        while bytes_recd < MSGLEN:
-            chunk = self.request.recv((MSGLEN - bytes_recd))
-            if chunk == b'':
-                raise RuntimeError("socket connection broken")
-            chunks.append(chunk)
-            bytes_recd = bytes_recd + len(chunk)
-        return b''.join(chunks)
-
-    def myheader(self): # my receive all for a consistent header of 2 BYTES
-        chunks = []
-        bytes_recd = 0
-        while bytes_recd < 2:
-            chunk = self.request.recv((2))
-            if chunk == b'':
-                raise RuntimeError("socket connection broken")
-            chunks.append(chunk)
-            bytes_recd = bytes_recd + len(chunk)
-            bytes_recd += 1
-        mynum = int.from_bytes(b"".join(chunks),'big')
-        return mynum
-
-    def mySend(self, msg): # abstracted out sendall to clean up the handler
-        header = (len(msg)).to_bytes(2,'big');
-        bmsg = msg.encode('utf-8')
-        self.request.sendall(header);
-        self.request.sendall(bmsg);
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("USAGE: chatserve.py PORT")
-        sys.exit(1)
-    HOST, PORT = socketserver.socket.gethostname(), int(sys.argv[1]) # get cmdline args
-
-    server = socketserver.TCPServer((HOST, PORT), MyTCPHandler) # start tcp server
-    signal.signal(signal.SIGINT, signal_handler) # use sig int to quit
-    print("Connection open on port " + HOST, PORT);
-    server.serve_forever() # keep server up
-
-
-
+print("Sent:     {}".format(data))
+print("Received: {}".format(received))
--- asg_2/ftserver.c
+++ asg_2/ftserver.c
@@ -1,115 +1,137 @@
 /*
-** showip.c -- show IP addresses for a host given on the command line
+erver.c -- a stream socket server demo
 */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <netdb.h>
 #include <arpa/inet.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <stdint.h>
-void error(const char *msg) { perror(msg); exit(0); } // Error function used for reporting issues
-int sendMsg(int, void*);
-int recvMsg(int, void*, int);
-int main(int argc, char *argv[])
-{
-    struct addrinfo hints, *res;
-    int status, sockfd;
+#include <sys/wait.h>
+#include <signal.h>
 
-    if (argc != 3) {
-        fprintf(stderr,"usage: showip hostname port\n");
+#define PORT "3490"  // the port users will be connecting to
+
+#define BACKLOG 10     // how many pending connections queue will hold
+
+void sigchld_handler(int s)
+{
+    // waitpid() might overwrite errno, so we save and restore it:
+    int saved_errno = errno;
+
+    while(waitpid(-1, NULL, WNOHANG) > 0);
+
+    errno = saved_errno;
+}
+
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int main(void)
+{
+    int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+    struct addrinfo hints, *servinfo, *p;
+    struct sockaddr_storage their_addr; // connector's address information
+    socklen_t sin_size;
+    struct sigaction sa;
+    int yes=1;
+    char s[INET6_ADDRSTRLEN];
+    int rv;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE; // use my IP
+
+    if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
         return 1;
     }
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC; // AF_INET or AF_INET6 to force version
-    hints.ai_socktype = SOCK_STREAM;
 
-    if ((status = getaddrinfo(argv[1], argv[2], &hints, &res)) != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
-        return 2;
-    }
-    sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-    if(connect(sockfd, res->ai_addr, res->ai_addrlen) == -1) {
-       error("FAILED TO CONNECT TO CLIENT: ");
-       }
-    printf("CONNECTED TO HOST: %s on PORT: %s\n", argv[1], argv[2]);
-    // vars to store our msgs
-    char clientMsg[501];
-    char hostMsg[501];
-    char clientName[11]; // 1 extra than max then trim off \n
-    char serverName[11];
-    memset((char*) &clientName, '\0', sizeof clientName); // zero out memory
-    printf("Please enter your name: ");
-    fgets(clientName, 11, stdin);
-    sendMsg(sockfd, clientName);
-    memset((char*) &serverName, '\0', sizeof serverName);
-    recvMsg(sockfd, serverName, 11);
-    char* pos;
-    if ((pos=strchr(clientName, '\n')) != NULL)
-        *pos = '\0';
-    printf("Connected with %s:\n", serverName);
-    while(1) {
-        memset((char*) &clientMsg, '\0', sizeof clientMsg);
-        memset((char*) &hostMsg, '\0', sizeof hostMsg);
-        printf("%s > ", clientName);
-        fgets(clientMsg, 501, stdin);
-        sendMsg(sockfd, clientMsg);
-        if(strcmp(clientMsg, "\\quit\n") == 0) { // if we fgets a quit then we will return a 0 and break loop
-            printf("Disconnecting from %s.\n", "Host");
-            break;
+    // loop through all the results and bind to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("server: socket");
+            continue;
         }
-        recvMsg(sockfd, hostMsg, 501);
-        if(strcmp(hostMsg, "\\quit") == 0) {
-            printf("%s has disconnected.\n", serverName);
-            break;
-        }
-        printf("%s > %s\n", serverName, hostMsg);
-    }
-    shutdown(sockfd,2);
-    freeaddrinfo(res); // free the linked list
-    return 0;
-}
-/* Loops recv so al data gets sent*/
-int sendMsg(int sockfd, void* buffer) {
-    int sent = 0; // holds the number of bytes sent
-    int totalSent = 0; // hold the total of bytes sent
-    unsigned short header = strlen(buffer);
-    unsigned short nheader = htons(header); // we are gonna send a 16bit header representing
-    do {                                               // the size of our msg
-        sent = send(sockfd, &nheader, (sizeof(int16_t) - totalSent), 0); // sends the necessary amount of bytes
-        totalSent = totalSent + sent;  // adds the number to total sent
-    } while(totalSent < 2); // if we didnt send all the bytes then loop till we do
-    sent = 0; // reset values for the message send
-    totalSent = 0;
-    do {
-       sent = send(sockfd, buffer, (header-totalSent), 0); //same process but with msg
-       totalSent = totalSent + sent;
-    } while(totalSent < header);
-    return 0;
-}
 
-/* A RecvAll function that also reads in a 2byte header before reading in a message*/
-int recvMsg(int sockfd, void* buffer, int sizeofBuffer) {
-    int recd = 0;
-    int totalRecd = 0;
-    unsigned short nheader, header;
-    do {
-        recd = recv(sockfd, &nheader, 2-totalRecd, 0); // loop till the header is received.. 2bytes unsigned shortl
-        totalRecd = totalRecd + recd;
-    } while(totalRecd < 2);
-    recd = 0;
-    totalRecd = 0;
-    header = ntohs(nheader); // convert from network byte
-    if(header > sizeofBuffer) { // if our header is bigger than our buffer then only read the size of the buffer
-        header = sizeofBuffer;
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+                sizeof(int)) == -1) {
+            perror("setsockopt");
+            exit(1);
+        }
+
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("server: bind");
+            continue;
+        }
+
+        break;
     }
-    do {
-        recd = recv(sockfd, buffer, (header-totalRecd), 0); // loop recv until we read a specifed bytes from the header
-        totalRecd = totalRecd + recd;
-    } while(totalRecd < header);
+    char hostname[1024];
+    hostname[1023] = '\0';
+    gethostname(hostname, 1023);
+    printf("Hostname: %s\n", hostname);
+    freeaddrinfo(servinfo); // all done with this structure
+
+    if (p == NULL)  {
+        fprintf(stderr, "server: failed to bind\n");
+        exit(1);
+    }
+
+    if (listen(sockfd, BACKLOG) == -1) {
+        perror("listen");
+        exit(1);
+    }
+
+    sa.sa_handler = sigchld_handler; // reap all dead processes
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+        perror("sigaction");
+        exit(1);
+    }
+
+    printf("server: waiting for connections...\n");
+
+    while(1) {  // main accept() loop
+        sin_size = sizeof their_addr;
+        printf("we here bby\n");
+        new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+        if (new_fd == -1) {
+            perror("accept");
+            continue;
+        }
+
+        inet_ntop(their_addr.ss_family,
+            get_in_addr((struct sockaddr *)&their_addr),
+            s, sizeof s);
+        printf("server: got connection from %s\n", s);
+
+        if (!fork()) { // this is the child process
+            close(sockfd); // child doesn't need the listener
+            if (send(new_fd, "Hello, world!", 13, 0) == -1)
+                perror("send");
+            close(new_fd);
+            exit(0);
+        }
+        close(new_fd);  // parent doesn't need this
+    }
+
     return 0;
 }
